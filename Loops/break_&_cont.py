78a796f701dc579be7cb7852d4bb49fbab83a4a0
Tricio14 <pv26841@estgl.ipv.pt>
2022-11-09 11:52:29 +0000

    break

---
+++ Loops/break_&_cont.py
@@ -0,0 +1,5 @@
+while True:
+    word = input("Your are stuck in an infinite loop!\nEnter The secrect word!")
+    if word == "chupacabra":
+        break
+print("You'successfully left the loop!")
---
+++ Loops/for.py
@@ -0,0 +1,2 @@
+for i in range(-999, 500):
+    print("The value of i is currently", i)
