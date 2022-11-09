c4ad55efb57c09ce574ff3b7d8b2cdc136859d27
Tricio14 <pv26841@estgl.ipv.pt>
2022-11-09 12:26:18 +0000

    word_without_vowels

---
+++ Loops/word_without_vowels.py
@@ -0,0 +1,18 @@
+word_whithout_vowels = ""
+user_word = input("Enter your word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    word_whithout_vowels += letter
+
+print(word_whithout_vowels)
